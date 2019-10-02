b73263ee7aed519e53210412e516f43e5cf48c94
Nahuel Balsamo <nbalsamo@elementum.com>
2019-10-02 13:30:22 -0300

    Armando al budita

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-/*
-    Temario:
-        1- Crear nodos de DOM desde JS, para editar el nombre del Budita.
-        2- Editar el color de piel del Budita
-        3- Cambiar el color de ropa.
-*/
+// Agregar un div con la clase buddha dentro del container con id buddha-container
 
-var buddhaContainerEl = document.getElementById('buddha-container');
-console.log(buddhaContainerEl);
+// Agregar un div con la clase head dentro del buddha creado en el paso anterior
+
+// Agregar un span con la clase hand dentro del buddha despues del div head
+
+// Agregar un span con la clase tail y otro span con la clase legs dentro del buddha pero antes que el div head
+
+// Agregar un span con la clase shadow luego del buddha
+
+// Agregar dos spans con la clase ears, un span con la clase circle, dos spans con la clase eye y un span con la clase mouth dentro del div head
+
+// Elegir la primera oreja y agregarle la class right
+
+// Elegir la segunda oreja y agregarle la class left
+
+// Elegir el primer ojo y agregarle la class right
+
+// Elegir el segundo ojo y agregarle la class left
+
+// Personalicen al budita con los colores que quieran y envien la foto al canal de slack de como quedo su budita
