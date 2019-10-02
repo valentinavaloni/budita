c2a69b5159032608689e233b04d0b840c507aaee
Diego Feresin <diegoferesin@gmail.com>
2019-10-01 23:45:19 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+    Temario:
+        1- Crear nodos de DOM desde JS, para editar el nombre del Budita.
+        2- Editar el color de piel del Budita
+        3- Cambiar el color de ropa.
+*/
+
+var buddhaContainerEl = document.getElementById('buddha-container');
+console.log(buddhaContainerEl);
